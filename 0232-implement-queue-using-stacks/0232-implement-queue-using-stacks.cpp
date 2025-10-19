9df469806b9b0f7d7e66b5c1fa302ad351b3a324
Kartikey Shukla <146567877+Kartikeychandrashukla@users.noreply.github.com>
2025-10-19 16:16:42 +0530

    Time: 0 ms (100%), Space: 9.7 MB (66.33%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,67 @@
+class MyQueue {
+    stack<int>st1;
+    stack<int>st2;
+public:
+    MyQueue() {
+        
+    }
+    
+    
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    int pop() {
+        if(st1.empty())
+        {
+            return -1;
+        }
+        while(st1.size()>1)
+        {
+            st2.push(st1.top());
+            st1.pop();
+        }
+        int ans=st1.top();
+        st1.pop();
+        while(!st2.empty())
+        {
+st1.push(st2.top());
+st2.pop();
+        }
+        return ans;
+    }
+    
+    int peek() {
+           if(st1.empty())
+        {
+            return -1;
+        }
+        while(st1.size()>1)
+        {
+            st2.push(st1.top());
+            st1.pop();
+        }
+        int ans=st1.top();
+        while(!st2.empty())
+        {
+st1.push(st2.top());
+st2.pop();
+        }
+        return ans;
+    }
+    
+    bool empty() {
+        if(st1.empty())
+        return true;
+        return false;
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
