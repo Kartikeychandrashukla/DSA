2a123e652d7f9d1b3e1094f3615e8670d6955ec3
Kartikey Shukla <146567877+Kartikeychandrashukla@users.noreply.github.com>
2025-10-25 00:42:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> prevPermOpt1(vector<int>& arr) {
+        int i=arr.size()-2;
+        while(i>=0 && arr[i]<=arr[i+1])
+        {
+            i--;
+        }
+        if(i>=0)
+        {
+            int j=arr.size()-1;
+            while(arr[j]>=arr[i]) {
+                j--;
+            }
+            swap(arr[i],arr[j]);
+        }
+
+return arr;
+
+    }
+};
