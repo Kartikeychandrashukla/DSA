c4f22bf791820cf69d11b821e7b5be3549a632f7
Kartikey Shukla <146567877+Kartikeychandrashukla@users.noreply.github.com>
2025-11-04 18:42:25 +0530

    Time: 8 ms (61.94%), Space: 44.7 MB (95.7%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int child=0;
+        int cookies=0;
+sort(g.begin(),g.end());
+sort(s.begin(),s.end());
+        while(child<g.size() && cookies<s.size())
+        {
+            if(g[child]<=s[cookies])
+            {
+                child++;
+                cookies++;
+            }
+            else
+            {
+                cookies++;
+            }
+        }
+        return child;
+     }
+};
