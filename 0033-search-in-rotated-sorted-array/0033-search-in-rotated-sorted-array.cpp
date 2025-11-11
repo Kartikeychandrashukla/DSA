3d3877dcabea960f5cf12e9124d6fbd3243dcfc3
Kartikey Shukla <146567877+Kartikeychandrashukla@users.noreply.github.com>
2025-11-11 20:23:34 +0530

    Time: 0 ms (100%), Space: 15.2 MB (65.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
-          int left=0;
-          int right=nums.size()-1;
-
-          while(left<=right)
-          {
-             int med=left+(right-left)/2;
-if(nums[med]==target)
-{
-    return med;
-}
-             if(nums[med]>=nums[left])
-             {
+        int left=0;
+        int right=nums.size()-1;
+                                                         // [4,5,6,0,1,2]
+        while(left<=right)
+        {
+            int med=left+(right-left)/2;
+            if(nums[med]==target)
+            {
+                return med;
+            }
+            else if(nums[med]>=nums[left])
+            {
                 if(nums[left]<=target && target<nums[med])
                 {
                     right=med-1;
                 }
                 else
                 {
-                    left=med+1;
+                        left=med+1;
                 }
-
-             }
-else
-{
-if(nums[med]<target && target<=nums[right])
-{
-    left=med+1;
-}
-else
-{
-    right=med-1;
-}
-
-}
-          }
-return -1;
-
+            } 
+            else
+            {
+                if(nums[med]<target && target<=nums[right])
+                {
+                     left=med+1;
+                }
+                else
+                {
+                   right=med-1;
+                }
+            }
+        }
+        return -1;
     }
 };
