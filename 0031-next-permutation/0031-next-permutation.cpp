3eb68ab9fd82d03d16d42d873cfbbc2b26a102d6
Kartikey Shukla <146567877+Kartikeychandrashukla@users.noreply.github.com>
2025-11-14 17:47:17 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.48%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int i=nums.size()-2;
-        while(i>=0 && nums[i]>=nums[i+1])
-        {
-            i--;
-        }
-if(i>=0)
-{
+      
+      int i=nums.size()-2;
+      while(i>=0 && nums[i]>=nums[i+1])
+      {
+        i--;
+      }
+      if(i>=0)
+      {
         int j=nums.size()-1;
-        while(nums[j]<=nums[i])j--;
-swap(nums[j],nums[i]);
+while(nums[j]<=nums[i])
+{
+    j--;
 }
-
-reverse(nums.begin()+i+1,nums.end());
-
-    }
+        swap(nums[i],nums[j]);
+      }
+        
+        reverse(nums.begin()+i+1,nums.end());
+       
+            }
 };
