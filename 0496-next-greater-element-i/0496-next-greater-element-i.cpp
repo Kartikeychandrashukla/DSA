857bb03176aa3a47e45336ce066e89da972a37dd
Kartikey Shukla <146567877+Kartikeychandrashukla@users.noreply.github.com>
2026-05-17 12:23:28 +0530

    Time: 4 ms (22.62%), Space: 13 MB (12.84%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,30 +3,33 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
         stack<int>st;
-        unordered_map<int,int>mp;
-for(int i=nums2.size()-1;i>=0;i--)
-{
-        while(!st.empty() && nums2[i]>st.top())
-        {
-            st.pop();
-        }
-        if(!st.empty())
-        {
-              mp[nums2[i]]=st.top();
-        }
-        else
-        {
-             mp[nums2[i]]=-1;
-        }
-        st.push(nums2[i]);
+     unordered_map<int,int>mp;
+vector<int>result;
+     for(int i=nums2.size()-1;i>=0;i--)
+     {
+       
+        
+    while(!st.empty() && st.top()<nums2[i])
+    {
+    st.pop();
     }
+   if(st.empty()) {
+    mp[nums2[i]] = -1;
+} else {
+    mp[nums2[i]] = st.top();   // ✅ only when stack is non-empty
+}
+  st.push(nums2[i]);
 
-vector<int>result(nums1.size(),0);
+     }
 
 for(int i=0;i<nums1.size();i++)
 {
-    result[i]=mp[nums1[i]];
+
+result.push_back(mp[nums1[i]]);
+
 }
+
 return result;
+
     }
 };
